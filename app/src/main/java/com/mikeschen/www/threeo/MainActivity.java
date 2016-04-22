0f5cf975f724b2adcbbf16b4f7aede7900806a90
Michael Schennum <mikeschenphoto@gmail.com>
2016-04-22 14:07:16 -0700

    adds font.ttf file

--- app/src/main/java/com/mikeschen/www/threeo/MainActivity.java
+++ app/src/main/java/com/mikeschen/www/threeo/MainActivity.java
@@ -2,6 +2,7 @@ package com.mikeschen.www.threeo;
 
 import android.content.ClipData;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
