185d9b6cf8c5e5da20133719ca93138af0bdada4
Michael Schennum <mikeschenphoto@gmail.com>
2016-04-22 13:55:41 -0700

    adds listview for articles

--- app/src/main/java/com/mikeschen/www/threeo/MainActivity.java
+++ app/src/main/java/com/mikeschen/www/threeo/MainActivity.java
@@ -10,12 +10,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    @Bind(R.id.contentListView) ListView mContentListView;
+    @Bind(R.id.fab) FloatingActionButton fab;
+    private String[] headlines = new String[] {"Uber Settles", "Apple Services in China", "F.B.I. Director Says 1.3 Mil For iPhone Hack", "Alphabet Earnings Disappoint", "Microsoft Cloud Biz Falls Short"};
     public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -23,21 +32,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.main_logo);
         toolbar.setTitle("");
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        fab.setOnClickListener(this);
+
+//        setContentView(R.layout.activity_post);
+//        mContentListView = (ListView) findViewById(R.id.contentListView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, headlines);
+        mContentListView.setAdapter(adapter);
+
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String post = ((TextView)view).getText().toString();
+//                Toast.makeText(MainActivity.this, post, Toast.LENGTH_LONG).show();
+//            }
+//        });
+
+//        @Override
+//        public void onClick(View view) {
+//            switch (view.getId()) {
+//                case R.id.fab:
+//                    Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+//                    startActivity(intent);
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
     }
 
     @Override
