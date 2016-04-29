95623952139e69a5c5b57d19bccf3c43c52d81f1
Michael Schennum <mikeschenphoto@gmail.com>
2016-04-29 09:51:39 -0700

    adds api uqyery

---
+++ app/src/main/java/com/mikeschen/www/threeo/services/FlickrService.java
@@ -0,0 +1,36 @@
+package com.mikeschen.www.threeo.services;
+
+import android.util.Log;
+
+import com.mikeschen.www.threeo.Constants;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by reviveit on 4/29/16.
+ */
+public class FlickrService {
+    public static void findPhotos(String headline, Callback callback) {
+        String CONSUMER_KEY = Constants.FLICKR_CONSUMER_KEY;
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.FLICKR_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.FLICKR_QUERY_PARAMETER, "flickr.photos.search");
+        urlBuilder.addQueryParameter(Constants.FLICKR_CONSUMER_KEY, "api_key");
+        urlBuilder.addQueryParameter("format", "json");
+        urlBuilder.addQueryParameter("nojsoncallback", "1");
+        urlBuilder.addQueryParameter(Constants.FLICKR_CONSUMER_KEY, "api_key");
+        urlBuilder.addQueryParameter("text", headline);
+        String url = urlBuilder.build().toString();
+        Log.d("string url", url);
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+}
