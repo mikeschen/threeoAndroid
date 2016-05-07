e1049073af9282079a133199bdbcd9f14df7fee6
Michael Schennum <mikeschenphoto@gmail.com>
2016-05-06 17:40:22 -0700

    adds firebase users

--- app/src/main/java/com/mikeschen/www/threeo/ui/CreateAccountActivity.java
+++ app/src/main/java/com/mikeschen/www/threeo/ui/CreateAccountActivity.java
@@ -3,17 +3,33 @@ package com.mikeschen.www.threeo.ui;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.mikeschen.www.threeo.Constants;
 import com.mikeschen.www.threeo.R;
+import com.mikeschen.www.threeo.models.User;
+
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class CreateAccountActivity extends Activity {
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.createUserButton)
+    Button mCreateUserButton;
     @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
     @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
+    private Firebase mFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +38,42 @@ public class CreateAccountActivity extends Activity {
         ButterKnife.bind(this);
         Typeface custom_font = Typeface.createFromAsset(getAssets(), "Imperial.ttf");
         mNameEditText.setTypeface(custom_font);
+        mEmailEditText.setTypeface(custom_font);
         mPasswordEditText.setTypeface(custom_font);
+        mConfirmPasswordEditText.setTypeface(custom_font);
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+        mCreateUserButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        if (view == mCreateUserButton) {
+            createNewUser();
+        }
+    }
+    public void createNewUser() {
+        final String name = mNameEditText.getText().toString();
+        final String email = mEmailEditText.getText().toString();
+        final String password = mPasswordEditText.getText().toString();
+        final String confirmPassword = mConfirmPasswordEditText.getText().toString();
+
+        mFirebaseRef.createUser(email, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
+            @Override
+            public void onSuccess(Map<String, Object> result) {
+                String uid = result.get("uid").toString();
+                createUserInFirebaseHelper(name, email, uid);
+            }
+
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                Log.d("Warning", "error occurred " +
+                        firebaseError);
+            }
+        });
+    }
+    private void createUserInFirebaseHelper(final String name, final String email, final String uid) {
+        final Firebase userLocation = new Firebase(Constants.FIREBASE_URL_USERS).child(uid);
+        User newUser = new User(name, email);
+        userLocation.setValue(newUser);
     }
 }
+
--- app/src/main/java/com/mikeschen/www/threeo/ui/LoginActivity.java
+++ app/src/main/java/com/mikeschen/www/threeo/ui/LoginActivity.java
@@ -2,9 +2,11 @@ package com.mikeschen.www.threeo.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.mikeschen.www.threeo.R;
@@ -13,14 +15,18 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    @Bind(R.id.registerTextView)
-    TextView mRegisterTextView;
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "Imperial.ttf");
+        mEmailEditText.setTypeface(custom_font);
+        mPasswordEditText.setTypeface(custom_font);
         mRegisterTextView.setOnClickListener(this);
     }
 
